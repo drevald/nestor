67acb37b1aad9ee08b744ad103f6552051d63502
Me <me@localhost>
2022-06-09 00:10:08 +0300

    game added

--- game/game.go
+++ game/game.go
@@ -1,16 +1,23 @@
 package game
 
 import (
-	"github.com/hajihoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func (g *Game) Init () {
 
 }
 
-func (g *Game) Draw () {
+func (g *Game) Layout (int, int) (int, int) {
+	return 100, 100
+}
+
+func (g *Game) Draw (*ebiten.Image) {
 
 }
 
+func (g *Game) Update () error {
+	return nil
+}
 
 type Game struct {}
