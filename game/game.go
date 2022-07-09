f9b1ec463e2b00ca01a69a4f9ebe157e2aecc228
Me <me@localhost>
2022-07-09 12:31:49 +0300

    chars encapsulated in object

--- game/game.go
+++ game/game.go
@@ -2,18 +2,32 @@ package game
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"image"
 	"image/color"
 	"os"
 	"fmt"
 	"bytes"
 	"image/png"
+	"math/rand"
 )
 
 // var r image.Rectangle
 
 type Game struct {
 	pos int
+	bukva Bukva
+}
+
+type Bukva struct {
+	bounds []int
+	char []rune
+}
+
+var bukvy = []Bukva{
+	{bounds:[]int{68, 37, 103, 109}, char:[]rune{'\u0410'}}, // азъ
+	{bounds:[]int{107, 41, 129, 96}, char:[]rune{'\u0430'}}, // азъ
+	{bounds:[]int{58, 177, 119, 234}, char:[]rune{'\u0431'}}, // буки
 }
 
 func (g *Game) Init () {
@@ -25,6 +39,9 @@ func (g *Game) Layout (int, int) (int, int) {
 }
 
 func (g *Game) Draw (screen *ebiten.Image) {
+	if g.bukva.bounds == nil {
+		g.bukva = bukvy[rand.Intn(3)]
+	}
     screen.Fill(color.RGBA{255, 255, 255, 255})
 	options := &ebiten.DrawImageOptions{}
 	options.GeoM.Translate(100.0, float64(g.pos%500))
@@ -41,12 +58,17 @@ func (g *Game) Draw (screen *ebiten.Image) {
 	}
 	
 	bukImage := ebiten.NewImageFromImage(buk)
-	subImage := bukImage.SubImage(image.Rect(68, 37, 103, 109)).(*ebiten.Image)
+	//subImage := bukImage.SubImage(image.Rect(68, 37, 103, 109)).(*ebiten.Image)
+	subImage := bukImage.SubImage(image.Rect(g.bukva.bounds[0], g.bukva.bounds[1], g.bukva.bounds[2], g.bukva.bounds[3])).(*ebiten.Image)
 	screen.DrawImage(subImage, options)
 	g.pos++
 
 }
 
 func (g *Game) Update () error {
+	if inpututil.IsKeyJustPressed(ebiten.KeyF) {
+		g.bukva.bounds = nil
+		g.bukva.char = nil
+	}	
     return nil
 }
